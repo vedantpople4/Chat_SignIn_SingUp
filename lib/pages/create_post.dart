9ba439b40759fd4401c842f197bc71ad512c33bf
vedantpople <vedantpople4@gmail.com>
2020-06-23 16:56:04 +0530

    New

--- lib/pages/create_post.dart
+++ lib/pages/create_post.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-class CreatePostPage extends StatefulWidget{
+class CreatePostPage extends StatefulWidget {
   @override
-  _CreatePostPage createState() => _CreatePostPage();
+  _CreatePostPageState createState() => _CreatePostPageState();
 }
 
-class _CreatePostPageState extends State<CreatePostPage>{
+class _CreatePostPageState extends State<CreatePostPage> {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Container(
       color: Colors.red,
     );
---
+++ lib/pages/notifications.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutterfirebase/models/post.dart';
+import 'package:flutterfirebase/models/user.dart';
+import 'package:flutterfirebase/models/comment.dart';
+import 'package:flutterfirebase/models/global.dart';
+
+class NotificationsPage extends StatefulWidget {
+  @override
+  _NotificationsPageState createState() => _NotificationsPageState();
+}
+
+class _NotificationsPageState extends State<NotificationsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+    );
+  }
+}
