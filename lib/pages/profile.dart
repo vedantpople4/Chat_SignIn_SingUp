ec53937c3087f920749698630ca0a6b3e4ecf0e3
vedantpople <vedantpople4@gmail.com>
2020-06-25 15:46:06 +0530

    New

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -23,7 +23,7 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
+      color: Colors.black,
       child: Column(
         children: <Widget>[
           Container(
