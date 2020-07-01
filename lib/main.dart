f2e521f87d5e20322271e8203b73ea17d62c6221
vedantpople <vedantpople4@gmail.com>
2020-07-01 16:21:13 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,56 +5,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Instagram',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<Widget> pages = [
-    //Homepage(),
-    SearchPage(),
-    //CreatpostPage(),
-    NotificationsPage(),
-    ProfilePage()
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 5,
-      initialIndex: 0,
-      child: Scaffold(
-        body: TabBarView(
-          children: pages,
-        ),
-        bottomNavigationBar: Container(
-          margin: EdgeInsets.only(bottom: 20.00),
-          child: new TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(Icons.home),
-              ),
-              Tab(
-                icon: Icon(Icons.search),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+    return MaterialApp();
   }
 }
