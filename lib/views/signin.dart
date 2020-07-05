e1c8ec2303cd8f142ba67f70125247f991db9dd6
vedantpople <vedantpople4@gmail.com>
2020-07-05 22:22:35 +0530

    Update signin.dart

--- lib/views/signin.dart
+++ lib/views/signin.dart
@@ -11,71 +11,75 @@ class _SignInState extends State<SignIn> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: appBarMain(context),
-        body: Container(
-            padding: EdgeInsets.symmetric(horizontal: 24),
-            child: Column(children: [
-              TextField(
-                  style: simpleTextFieldStyle(),
-                  decoration: textFieldInputDecoration("email")),
-              TextField(
-                  style: simpleTextFieldStyle(),
-                  decoration: textFieldInputDecoration("password")),
-              SizedBox(
-                height: 8,
-              ),
-              Container(
-                alignment: Alignment.centerRight,
-                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                child: Text("Forgot password", style: simpleTextFieldStyle()),
-              ),
-              SizedBox(
-                height: 8,
-              ),
-              Container(
-                alignment: Alignment.center,
-                width: MediaQuery.of(context).size.width,
-                padding: EdgeInsets.symmetric(vertical: 20),
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(colors: [
-                    const Color(0xff007EF4),
-                    const Color(0xff2A75BC)
-                  ]),
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                child: Text("Sign In", style: mediumTextFieldStyle()),
-              ),
-              SizedBox(
-                height: 8,
-              ),
-              Container(
-                alignment: Alignment.center,
-                width: MediaQuery.of(context).size.width,
-                padding: EdgeInsets.symmetric(vertical: 20),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                child: Text("Sign In with Google",
-                    style: TextStyle(
+        body: SingleChildScrollView(
+            child: Container(
+                alignment: Alignment.bottomCenter,
+                height: 50.00,
+                padding: EdgeInsets.symmetric(horizontal: 24),
+                child: Column(children: [
+                  TextField(
+                      style: simpleTextFieldStyle(),
+                      decoration: textFieldInputDecoration("email")),
+                  TextField(
+                      style: simpleTextFieldStyle(),
+                      decoration: textFieldInputDecoration("password")),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    child:
+                        Text("Forgot password", style: simpleTextFieldStyle()),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(colors: [
+                        const Color(0xff007EF4),
+                        const Color(0xff2A75BC)
+                      ]),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Text("Sign In", style: mediumTextFieldStyle()),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    decoration: BoxDecoration(
                       color: Colors.white,
-                      fontSize: 17,
-                    )),
-              ),
-              SizedBox(
-                height: 8,
-              ),
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text("Dont have an account? ",
-                        style: mediumTextFieldStyle()),
-                    Text("Register now",
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Text("Sign In with Google",
                         style: TextStyle(
                           color: Colors.white,
                           fontSize: 17,
-                          decoration: TextDecoration.underline,
                         )),
-                  ])
-            ])));
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text("Dont have an account? ",
+                            style: mediumTextFieldStyle()),
+                        Text("Register now",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 17,
+                              decoration: TextDecoration.underline,
+                            )),
+                      ])
+                ]))));
   }
 }
