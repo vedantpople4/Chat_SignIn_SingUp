50ade45111f29605a4af2ebd234d0f3627538ac6
vedantpople <vedantpople4@gmail.com>
2020-07-06 15:30:10 +0530

    new

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -3,5 +3,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  Future signInWithEmailandPassword(String email, String password) {}
+  Future signInWithEmailandPassword(String email, String password) {
+    try {
+      AuthResult result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+    } catch (e) {}
+  }
 }
