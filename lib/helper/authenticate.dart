313e8507ddb8babfb262e55211b1164ca2ccf99b
vedantpople <vedantpople4@gmail.com>
2020-07-08 17:26:04 +0530

    Update authenticate.dart

--- lib/helper/authenticate.dart
+++ lib/helper/authenticate.dart
@@ -1,1 +1,13 @@
-import 'package:'
+import 'package:flutter/material.dart';
+
+class Authenticate extends StatefulWidget {
+  @override
+  _AuthenticateState createState() => _AuthenticateState();
+}
+
+class _AuthenticateState extends State<Authenticate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
