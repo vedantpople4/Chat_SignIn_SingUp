a8863f823896a7895863384a6b15aafc4beb4334
vedantpople <vedantpople4@gmail.com>
2020-07-10 15:28:16 +0530

    new

--- lib/views/search.dart
+++ lib/views/search.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutterfirebase/widgets/widget.dart';
 
 class SearchScreen extends StatefulWidget {
   @override
@@ -6,8 +7,51 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  TextEditingController searchTextEditingController =
+      new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: appBarMain(context),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              color: Color(0x54FFFFFF),
+              padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                      child: TextField(
+                    controller: searchTextEditingController,
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      hintText: "search username..",
+                      hintStyle: TextStyle(
+                        color: Colors.white54,
+                      ),
+                      border: InputBorder.none,
+                    ),
+                  )),
+                  Container(
+                    height: 40,
+                    width: 40,
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(colors: [
+                          const Color(0x36FFFFFF),
+                          const Color(0x0FFFFFFF)
+                        ]),
+                        borderRadius: BorderRadius.circular(40)),
+                    padding: EdgeInsets.all(12),
+                    child: Image.asset("assets/images/search_white.png"),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
