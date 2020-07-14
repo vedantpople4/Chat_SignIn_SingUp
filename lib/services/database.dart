bed7a0488f799e68d902a49efc3ed1cdad8d9da1
vedantpople <vedantpople4@gmail.com>
2020-07-14 14:34:57 +0530

    database update

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,6 +1,49 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutterfirebase/views/chatRoomScreen.dart';
 
 class DatabaseMethods {
+  Future<void> addUserInfo(userData) async {
+    Firestore.instance.collection("users").add(userData).catchError((e) {
+      print(e.toString());
+    });
+  }
+
+  getUserInfo(String email) async {
+    return Firestore.instance
+        .collection("users")
+        .where("userEmail", isEqualTo: email)
+        .getDocuments()
+        .catchError((e) {
+      print(e.toString());
+    });
+  }
+
+  searchByName(String searchField) {
+    return Firestore.instance
+        .collection("users")
+        .where('userName', isEqualTo: searchField)
+        .getDocuments();
+  }
+
+  Future<bool> addChatRoom(chatRoom, chatRoomId) {
+    Firestore.instance
+        .collection("chatRoom")
+        .document(chatRoomId)
+        .setData(chatRoom)
+        .catchError((e) {
+      print(e);
+    });
+  }
+
+  getChats(String chatRoomId) async {
+    return Firestore.instance
+        .collection("chatRoom")
+        .document(chatRoomId)
+        .collection("chats")
+        .orderBy('time')
+        .snapshots();
+  }
+
   getUserByUsername(String username) async {
     return await Firestore.instance
         .collection("users")
