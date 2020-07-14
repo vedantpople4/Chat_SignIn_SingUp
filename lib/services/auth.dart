3975a6e56abf050f95642e09a0f23129d3bf264d
vedantpople <vedantpople4@gmail.com>
2020-07-14 14:19:46 +0530

    New

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -46,11 +46,30 @@ class AuthMethods {
 
   Future<FirebaseUser> signInWithGoogle(BuildContext context) async {
     final GoogleSignIn _googleSignIn = new GoogleSignIn();
+
+    final GoogleSignInAccount googleSignInAccount =
+        await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount.authentication;
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+        idToken: googleSignInAuthentication.idToken,
+        accessToken: googleSignInAuthentication.accessToken);
+
+    AuthResult result = await _auth.signInWithCredential(credential);
+    FirebaseUser userDetails = result.user;
+
+    if (result == null) {
+    } else {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => Chat()));
+    }
   }
 
   Future signOut() async {
     try {
       return await _auth.signOut();
-    } catch (e) {}
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
   }
 }
