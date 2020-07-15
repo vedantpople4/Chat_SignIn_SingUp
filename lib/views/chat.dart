9c13dfd464873fca59b355854914ba4c27f6b014
vedantpople <vedantpople4@gmail.com>
2020-07-15 14:40:01 +0530

    Chat update

---
+++ lib/views/chat.dart
@@ -0,0 +1,74 @@
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterfirebase/helper/constants.dart';
+import 'package:flutterfirebase/services/database.dart';
+import 'package:flutterfirebase/widgets/widget.dart';
+
+class Chat extends StatefulWidget {
+  final String charRoomId;
+
+  Chat({this.charRoomId});
+
+  @override
+  _ChatState createState() => _ChatState();
+}
+
+class _ChatState extends State<Chat> {
+  Stream<QuerySnapshot> chats;
+  TextEditingController messageEditingController = new TextEditingController();
+
+  Widget chatMessages() {
+    return StreamBuilder(
+      stream: chats,
+      builder: (context, snapshot) {
+        return snapshot.hasData
+            ? ListView.builder(
+                itemCount: snapshot.data.documents.length,
+                itemBuilder: (context, index) {
+                  return MessageTile(
+                    message: snapshot.data.documents[index].data["message"],
+                    sendBy: Constants.myName ==
+                        snapshot.data.documents[index].data["sendBy"],
+                  );
+                })
+            : Container();
+      },
+    );
+  }
+
+  addMessage() {
+    if (messageEditingController.text.isNotEmpty) {
+      Map<String, dynamic> chatMessageMap = {
+        "sendBy": Constants.myName,
+        "message": messageEditingController.text,
+        'time': DateTime.now().millisecondsSinceEpoch,
+      };
+      DatabaseMethods().addMessage(widget.chatRoomId, chatMessageMap);
+      setState(() {
+        messageEditingController.text = "";
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    DatabaseMethods().getChats(widget.chatRoomId).then((val) {
+      setState(() {
+        chats = val;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBarMain(context),
+      body: Container(
+        child: Stack,
+      ),
+    );
+  }
+}
